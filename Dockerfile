0006fcdfa29bc8a67658229ca1dc00722d52774c
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2024-02-27 14:58:36 +0100

    try to switch from nginx to Caddy

--- Dockerfile
+++ Dockerfile
@@ -2,19 +2,15 @@ FROM node:alpine as build-stage
 WORKDIR /app
 RUN yarn global add @quasar/cli
 COPY --from=top-base /base/top-api.tgz /app/top-api.tgz
-#COPY package.json yarn.lock .npmrc ./
 COPY package.json yarn.lock ./
 RUN yarn
 COPY . .
 RUN quasar build
 
-FROM nginx:alpine as production-stage
+FROM caddy:alpine as production-stage
 COPY --from=build-stage /app/dist/spa /opt/top-frontend
-COPY nginx.conf /etc/nginx/conf.d/default.conf
-COPY docker-entrypoint.d/* /docker-entrypoint.d/
-RUN chmod u+x /docker-entrypoint.d/*.sh
-RUN sed -i "s|notice|error|" /etc/nginx/nginx.conf
+COPY Caddyfile /etc/caddy/Caddyfile
+#COPY docker-entrypoint.d/* /docker-entrypoint.d/
+#RUN chmod u+x /docker-entrypoint.d/*.sh
 
 EXPOSE 80
-ENV NGINX_ENTRYPOINT_QUIET_LOGS=1
-CMD ["nginx", "-g", "daemon off;"]
--- docker-entrypoint.d/01-set-env-vars.sh
+++ docker-entrypoint.d/01-set-env-vars.sh
@@ -1,49 +1,49 @@
 if [[ ! -z "${OAUTH2_ENABLED}" ]]
 then
-    sed -i "s|OAUTH2_ENABLED\\:!1|OAUTH2_ENABLED:${OAUTH2_ENABLED}|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|OAUTH2_ENABLED\\:!1|OAUTH2_ENABLED:${OAUTH2_ENABLED}|g" /opt/top-frontend/js/app.*.js
 fi
 
 if [[ ! -z "${API_URL}" ]]
 then
-    sed -i "s|API_URL\\:\"http\\://127\\.0\\.0\\.1\\:8080\"|API_URL:'${API_URL}'|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|API_URL\\:\"http\\://127\\.0\\.0\\.1\\:8080\"|API_URL:'${API_URL}'|g" /opt/top-frontend/js/app.*.js
 fi
 
 if [[ ! -z "${OAUTH2_URL}" ]]
 then
-    sed -i "s|OAUTH2_URL\\:\"http\\://127\\.0\\.0\\.1\\:8081\"|OAUTH2_URL:'${OAUTH2_URL}'|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|OAUTH2_URL\\:\"http\\://127\\.0\\.0\\.1\\:8081\"|OAUTH2_URL:'${OAUTH2_URL}'|g" /opt/top-frontend/js/app.*.js
 fi
 
 if [[ ! -z "${OAUTH2_REALM}" ]]
 then
-    sed -i "s|OAUTH2_REALM\\:\"top-realm\"|OAUTH2_REALM:'${OAUTH2_REALM}'|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|OAUTH2_REALM\\:\"top-realm\"|OAUTH2_REALM:'${OAUTH2_REALM}'|g" /opt/top-frontend/js/app.*.js
 fi
 
 if [[ ! -z "${OAUTH2_CLIENT_ID}" ]]
 then
-    sed -i "s|OAUTH2_CLIENT_ID\\:\"top-frontend\"|OAUTH2_CLIENT_ID:'${OAUTH2_CLIENT_ID}'|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|OAUTH2_CLIENT_ID\\:\"top-frontend\"|OAUTH2_CLIENT_ID:'${OAUTH2_CLIENT_ID}'|g" /opt/top-frontend/js/app.*.js
 fi
 
 if [[ ! -z "${GDPR_NOTICE}" ]]
 then
-    sed -i "s|GDPR_NOTICE\\:!1|GDPR_NOTICE:${GDPR_NOTICE}|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|GDPR_NOTICE\\:!1|GDPR_NOTICE:${GDPR_NOTICE}|g" /opt/top-frontend/js/app.*.js
 fi
 
 if [[ ! -z "${GDPR_POLICY_URL}" ]]
 then
-    sed -i "s|GDPR_POLICY_URL\\:void 0|GDPR_POLICY_URL:'${GDPR_POLICY_URL}'|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|GDPR_POLICY_URL\\:void 0|GDPR_POLICY_URL:'${GDPR_POLICY_URL}'|g" /opt/top-frontend/js/app.*.js
 fi
 
 if [[ ! -z "${SYSTEM_NOTICE}" ]]
 then
-    sed -i "s|SYSTEM_NOTICE\\:void 0|SYSTEM_NOTICE:'${SYSTEM_NOTICE}'|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|SYSTEM_NOTICE\\:void 0|SYSTEM_NOTICE:'${SYSTEM_NOTICE}'|g" /opt/top-frontend/js/app.*.js
 fi
 
 if [[ ! -z "${DOCUMENTS_ENABLED}" ]]
 then
-    sed -i "s|DOCUMENTS_ENABLED\\:!1|DOCUMENTS_ENABLED:${DOCUMENTS_ENABLED}|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|DOCUMENTS_ENABLED\\:!1|DOCUMENTS_ENABLED:${DOCUMENTS_ENABLED}|g" /opt/top-frontend/js/app.*.js
 fi
 
 if [[ ! -z "${TOP_PHENOTYPIC_QUERY_DOC_BASE_URL}" ]]
 then
-    sed -i "s|TOP_PHENOTYPIC_QUERY_DOC_BASE_URL\\:\"https\\://onto-med\\.github\\.io/top-phenotypic-query/care/smith/top/top_phenotypic_query/c2reasoner\"|TOP_PHENOTYPIC_QUERY_DOC_BASE_URL:'${TOP_PHENOTYPIC_QUERY_DOC_BASE_URL}'|g" /usr/share/nginx/html/js/app.*.js
+    sed -i "s|TOP_PHENOTYPIC_QUERY_DOC_BASE_URL\\:\"https\\://onto-med\\.github\\.io/top-phenotypic-query/care/smith/top/top_phenotypic_query/c2reasoner\"|TOP_PHENOTYPIC_QUERY_DOC_BASE_URL:'${TOP_PHENOTYPIC_QUERY_DOC_BASE_URL}'|g" /opt/top-frontend/js/app.*.js
 fi
